df960746331e3f0b60fdc3ba05f9557d34fab10c
RISHABH CHOUDHARY <rishabhchoudhary028@gmail.com>
2026-09-14 00:44:33 +0530

    message

--- app/layout.js
+++ app/layout.js
@@ -1,16 +1,25 @@
 import "./global.css";
-
-export const metadata = {
-  title: "CYBERTRACE",
-  description: "AI-powered cybercrime monitoring and predictive risk analysis",
-};
+import Sidebar from "../components/sidebar";
+import Header from "../components/header";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <div className="app-layout">
+
+          <Sidebar />
+
+          <div className="page-area">
+            <Header />
+
+            <main className="main-content">
+              {children}
+            </main>
+          </div>
+
+        </div>
+      </body>
     </html>
   );
 }
-
-
